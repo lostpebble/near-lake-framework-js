fec2d24648aacab77c4598cee90d41942bbba0c9
lostpebble <lostpebble@protonmail.com>
2022-10-18 19:21:47 +0200

    Fixed property naming option things

--- src/streamer.ts
+++ src/streamer.ts
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { listBlocks, fetchStreamerMessage } from "./s3fetchers";
-import { LakeConfig, BlockHeight, StreamerMessage } from "./types";
+import { fetchStreamerMessage, listBlocks } from "./s3fetchers";
+import { EPropertyNamingFormat, LakeConfig, StreamerMessage } from "./types";
 import { sleep } from "./utils";
 
 async function* batchStream(
@@ -9,8 +9,12 @@ async function* batchStream(
   const s3Client = new S3Client({
     region: config.s3RegionName,
     endpoint: config.s3Endpoint,
-    forcePathStyle: config.s3ForcePathStyle
+    forcePathStyle: config.s3ForcePathStyle,
   });
+
+  if (config.propertyNamingFormat == null) {
+    config.propertyNamingFormat = EPropertyNamingFormat.javascript;
+  }
 
   let startBlockHeight = config.startBlockHeight;
 
@@ -36,19 +40,29 @@ async function* batchStream(
       continue;
     }
 
-    yield blockHeights.map(blockHeight => fetchStreamerMessage(s3Client, config.s3BucketName, blockHeight));
+    yield blockHeights.map((blockHeight) =>
+      fetchStreamerMessage(
+        s3Client,
+        config.s3BucketName,
+        blockHeight,
+        config.propertyNamingFormat!
+      )
+    );
     startBlockHeight = Math.max.apply(Math, blockHeights) + 1;
   }
 }
 
-async function* fetchAhead<T>(seq: AsyncIterable<T>, stepsAhead = 10): AsyncIterableIterator<T> {
+async function* fetchAhead<T>(
+  seq: AsyncIterable<T>,
+  stepsAhead = 10
+): AsyncIterableIterator<T> {
   let queue = [];
   while (true) {
     while (queue.length < stepsAhead) {
       queue.push(seq[Symbol.asyncIterator]().next());
     }
 
-    const {value, done} = await queue.shift();
+    const { value, done } = await queue.shift();
     if (done) return;
     yield value;
   }
@@ -57,14 +71,16 @@ async function* fetchAhead<T>(seq: AsyncIterable<T>, stepsAhead = 10): AsyncIter
 export async function* stream(
   config: LakeConfig
 ): AsyncIterableIterator<StreamerMessage> {
-  const s3Client = new S3Client({region: config.s3RegionName});
+  const s3Client = new S3Client({ region: config.s3RegionName });
 
   let lastProcessedBlockHash: string;
   let startBlockHeight = config.startBlockHeight;
 
   while (true) {
     try {
-      for await (let promises of fetchAhead(batchStream({...config, startBlockHeight}))) {
+      for await (let promises of fetchAhead(
+        batchStream({ ...config, startBlockHeight })
+      )) {
         for (let promise of promises) {
           const streamerMessage = await promise;
           // check if we have `lastProcessedBlockHash` (might be not set only on start)
@@ -76,7 +92,8 @@ export async function* stream(
             lastProcessedBlockHash !== streamerMessage.block.header.prevHash
           ) {
             throw new Error(
-              `The hash of the last processed block ${lastProcessedBlockHash} doesn't match the prevHash ${streamerMessage.block.header.prevHash} of the new one ${streamerMessage.block.header.hash}.`);
+              `The hash of the last processed block ${lastProcessedBlockHash} doesn't match the prevHash ${streamerMessage.block.header.prevHash} of the new one ${streamerMessage.block.header.hash}.`
+            );
           }
 
           yield streamerMessage;
@@ -87,7 +104,11 @@ export async function* stream(
       }
     } catch (e) {
       // TODO: Should there be limit for retries?
-      console.log('Retrying on error when fetching blocks', e, 'Refetching the data from S3 in 200ms');
+      console.log(
+        "Retrying on error when fetching blocks",
+        e,
+        "Refetching the data from S3 in 200ms"
+      );
       await sleep(200);
     }
   }
@@ -99,15 +120,18 @@ interface IStreamContext {
 
 export function startStream(
   config: LakeConfig,
-  onStreamerMessageReceived: (data: StreamerMessage, ctx: IStreamContext) => Promise<void>
-): (() => void) {
+  onStreamerMessageReceived: (
+    data: StreamerMessage,
+    ctx: IStreamContext
+  ) => Promise<void>
+): () => void {
   const streamState = {
     cancelled: false,
   };
 
   const stopStream = () => {
     streamState.cancelled = true;
-  }
+  };
 
   const ctx: IStreamContext = {
     stopStream,
